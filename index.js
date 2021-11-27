54bd7a8ccd68cfa120068180fef83832b96eee44
roobottom <roo@roodesign.co.uk>
2021-11-27 17:05:07 +0000

    undo local preset

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-import 'dotenv/config.js'
-import {Indiekit} from '@indiekit/indiekit'
-import {GithubStore} from '@indiekit/store-github'
+import 'dotenv/config.js';
+import {Indiekit} from '@indiekit/indiekit';
+import {JekyllPreset} from '@indiekit/preset-jekyll';
+import {GithubStore} from '@indiekit/store-github';
 
 // Create a new indiekit instance
-const indiekit = new Indiekit()
+const indiekit = new Indiekit();
+
+// Configure publication preset
+const jekyll = new JekyllPreset();
 
 // Configure content store
 const github = new GithubStore({
@@ -11,14 +15,14 @@ const github = new GithubStore({
   repo: 'roobottom-2021', // Repository files will be saved to
   branch: 'main', // Branch to publish to
   token:  process.env.GITHUB_TOKEN// GitHub personal access token
-})
+});
 
 // Application settings
 //indiekit.set('application.mongodbUrl', process.env.MONGODB_URL)
 
 // Publication settings
-indiekit.set('publication.locale', 'en-GB')
-indiekit.set('publication.me', 'https://roobottom.com')
+indiekit.set('publication.locale', 'en-GB');
+indiekit.set('publication.me', 'https://roobottom.com');
 indiekit.set('publication.postTypes', [
   {
     type: 'photo',
@@ -44,48 +48,13 @@ indiekit.set('publication.postTypes', [
       url: 'images/articles/{slug}/{filename}'
     }
   }
-])
-indiekit.set('publication.postTemplate', properties => {
-  let content
-  if (properties.content) {
-    content = properties.content.text || properties.content.html || properties.content
-    content = `${content}\n`
-  } else {
-    content = ''
-  }
-
-  properties = {
-    date: properties.published,
-    ...(properties.name && {title: properties.name}),
-    ...(properties.summary && {excerpt: properties.summary}),
-    ...(properties.category && {category: properties.category}),
-    ...(properties.start && {start: properties.start}),
-    ...(properties.end && {end: properties.end}),
-    ...(properties.rsvp && {rsvp: properties.rsvp}),
-    ...(properties.location && {location: properties.location}),
-    ...(properties.checkin && {checkin: properties.checkin}),
-    ...(properties.audio && {audio: properties.audio}),
-    ...(properties.photo && {photo: properties.photo}),
-    ...(properties.video && {video: properties.video}),
-    ...(properties['bookmark-of'] && {'bookmark-of': properties['bookmark-of']}),
-    ...(properties['like-of'] && {'like-of': properties['like-of']}),
-    ...(properties['repost-of'] && {'repost-of': properties['repost-of']}),
-    ...(properties['in-reply-to'] && {'in-reply-to': properties['in-reply-to']}),
-    ...(properties['post-status'] === 'draft' && {draft: true}),
-    ...(properties.visibility && {visibility: properties.visibility}),
-    ...(properties.syndication && {syndication: properties.syndication}),
-    ...(properties['mp-syndicate-to'] && {'mp-syndicate-to': properties['mp-syndicate-to']})
-  }
-  let frontmatter = YAML.stringify(properties)
-  frontmatter = `---\n${frontmatter}---\n`
-
-  return frontmatter + content
-})
-indiekit.set('publication.store', github)
-indiekit.set('publication.timeZone', 'Europe/London')
+]);
+indiekit.set('publication.preset', jekyll);
+indiekit.set('publication.store', github);
+indiekit.set('publication.timeZone', 'Europe/London');
 
 // Create a server
-const server = indiekit.server()
+const server = indiekit.server();
 
 // Export server
-export default server
+export default server;
