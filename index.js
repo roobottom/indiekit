839d76e25552e2a0e1b5ea8a88f9758e855577a3
roobottom <roo@roodesign.co.uk>
2021-12-19 22:19:26 +0000

    twitter syndication

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-import 'dotenv/config.js';
-import {Indiekit} from '@indiekit/indiekit';
-import {roobottomPreset} from './preset-roobottom-com.js';
-import {GithubStore} from '@indiekit/store-github';
+import 'dotenv/config.js'
+import {Indiekit} from '@indiekit/indiekit'
+import {roobottomPreset} from './preset-roobottom-com.js'
+import {GithubStore} from '@indiekit/store-github'
+import {TwitterSyndicator} from '@indiekit/syndicator-twitter'
 
 // Create a new indiekit instance
-const indiekit = new Indiekit();
+const indiekit = new Indiekit()
 
 // Configure publication preset
-const roobottomSiteSchema = new roobottomPreset();
+const roobottomSiteSchema = new roobottomPreset()
 
 // Configure content store
 const github = new GithubStore({
@@ -15,18 +16,31 @@ const github = new GithubStore({
   repo: 'roobottom-2021', // Repository files will be saved to
   branch: 'main', // Branch to publish to
   token:  process.env.GITHUB_TOKEN// GitHub personal access token
-});
+})
+
+// Syndication 
+const twitter = new TwitterSyndicator({
+  checked: true,
+  user: 'roobottom',
+  apiKey: process.env.TWTWITTER_APIKEY,
+  apiKeySecret: process.env.TWITTER_APISECRET,
+  accessToken: process.env.TWITTER_ACCESSTOKEN,
+  accessTokenSecret: process.env.TWITTER_ACCESSTOKENSECRET
+})
 
 // Publication settings
-indiekit.set('publication.locale', 'en-GB');
-indiekit.set('publication.me', 'https://roobottom.com');
-indiekit.set('publication.preset', roobottomSiteSchema);
-indiekit.set('publication.store', github);
-indiekit.set('publication.timeZone', 'Europe/London');
-indiekit.set('publication.categories', 'https://roobottom.com/tags/list.json');
+indiekit.set('publication.locale', 'en-GB')
+indiekit.set('publication.me', 'https://roobottom.com')
+indiekit.set('publication.preset', roobottomSiteSchema)
+indiekit.set('publication.store', github)
+indiekit.set('publication.timeZone', 'Europe/London')
+indiekit.set('publication.categories', 'https://roobottom.com/tags/list.json')
+indiekit.set('publication.syndicationTargets', [
+  twitter
+])
 
 // Create a server
-const server = indiekit.server();
+const server = indiekit.server()
 
 // Export server
-export default server;
+export default server
